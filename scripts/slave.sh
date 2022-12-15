a835a729d849954e56deb69b1e3e9713b67396a6
khanh <kenkukhanh@gmail.com>
2022-12-15 15:55:01 +0700

    slave file

--- scripts/slave.sh
+++ scripts/slave.sh
@@ -9,11 +9,11 @@ REDIS_ROOT_PASS=${REDIS_ROOT_PASS:-password}
 # Input IP master change "dig +short master"
 
 if [ -z $IP_MASTER ]; then
-  IP_MASTER=$(dig +short master)
+  IP_MASTER=${IP_MASTER:-192.168.12.162}
 fi
 
 # If use docker run mysql no need use service redis-server start
-#service redis-server start
+#sudo service redis-server start
 
 while :; 
 do
@@ -36,22 +36,22 @@ do
   fi
 done
 
-sed -i -E 's/^protected-mode no$/protected-mode yes/g' /etc/redis/redis.conf
+sudo sed -i -E 's/^protected-mode no$/protected-mode yes/g' /etc/redis/redis.conf
 
 #echo 'bind 0.0.0.0' >> /etc/redis/redis.conf
-echo "masterauth \"$REDIS_ROOT_PASS\"" | tee -a /etc/redis/redis.conf
-echo "slaveof  \"$IP_MASTER\" 6379" | tee -a /etc/redis/redis.conf
-echo "requirepass \"$REDIS_ROOT_PASS\"" | tee -a /etc/redis/redis.conf
+echo "masterauth \"$REDIS_ROOT_PASS\"" | sudo tee -a /etc/redis/redis.conf
+echo "slaveof  \"$IP_MASTER\" 6379" | sudo tee -a /etc/redis/redis.conf
+echo "requirepass \"$REDIS_ROOT_PASS\"" | sudo tee -a /etc/redis/redis.conf
 
 
-service redis-server restart
+sudo service redis-server restart
 
-echo "sentinel monitor redis-master \"$IP_MASTER\" 6379 2" | tee -a /etc/redis/sentinel.conf
-echo 'sentinel down-after-milliseconds redis-master 1500' | tee -a /etc/redis/sentinel.conf
-echo 'sentinel failover-timeout redis-master 3000' | tee -a /etc/redis/sentinel.conf
-echo 'protected-mode no' | tee -a /etc/redis/sentinel.conf
+echo "sentinel monitor redis-master \"$IP_MASTER\" 6379 2" | sudo tee -a /etc/redis/sentinel.conf
+echo 'sentinel down-after-milliseconds redis-master 1500' | sudo tee -a /etc/redis/sentinel.conf
+echo 'sentinel failover-timeout redis-master 3000' | sudo tee -a /etc/redis/sentinel.conf
+echo 'protected-mode no' | sudo tee -a /etc/redis/sentinel.conf
 
-service redis-sentinel restart
+sudo service redis-sentinel restart
 
-echo 'info replication' | redis-cli -a password
-echo 'info sentinel' | redis-cli -p 26379
+echo 'info replication' | sudo redis-cli -a password
+echo 'info sentinel' | sudo redis-cli -p 26379
